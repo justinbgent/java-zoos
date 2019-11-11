6398966bb9e669dc3f683740d6a434374ecb2c4a
Justin Gent <justinbgent@gmail.com>
2019-11-11 14:43:02 -0700

    Create model classes with properties and table relationships

---
+++ src/main/java/com/schoolwork/zoos/model/Zoo.java
@@ -0,0 +1,65 @@
+package com.schoolwork.zoos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "zoos")
+public class Zoo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long zooid;
+
+    private String zooname;
+
+    @ManyToMany(mappedBy = "zoos")
+    @JsonIgnoreProperties("zoos")
+    private List<Animal> animals = new ArrayList<>();
+
+    @OneToMany(mappedBy = "zoo", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("zoo")
+    private List<Telephone> telephones = new ArrayList<>();
+
+    public Zoo() {
+    }
+
+    public Zoo(String zooname) {
+        this.zooname = zooname;
+    }
+
+    public long getZooid() {
+        return zooid;
+    }
+
+    public void setZooid(long zooid) {
+        this.zooid = zooid;
+    }
+
+    public String getZooname() {
+        return zooname;
+    }
+
+    public void setZooname(String zooname) {
+        this.zooname = zooname;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Telephone> getTelephones() {
+        return telephones;
+    }
+
+    public void setTelephones(List<Telephone> telephones) {
+        this.telephones = telephones;
+    }
+}
---
+++ src/test/java/com/schoolwork/zoos/ZoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolwork.zoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
