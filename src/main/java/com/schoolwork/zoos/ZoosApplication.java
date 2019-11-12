d293e3bb96e0b910758ec361692b8e46c90cacec
Justin Gent <justinbgent@gmail.com>
2019-11-12 16:53:17 -0700

    Add ability to intercept errors normally handled by spring in main method

--- src/main/java/com/schoolwork/zoos/ZoosApplication.java
+++ src/main/java/com/schoolwork/zoos/ZoosApplication.java
@@ -2,12 +2,21 @@ package com.schoolwork.zoos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
 public class ZoosApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZoosApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ZoosApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) context.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
