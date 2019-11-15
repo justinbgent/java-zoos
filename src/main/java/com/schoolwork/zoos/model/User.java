8e729bccadef9098835c98dfe28dbeea43ae7192
Justin Gent <justinbgent@gmail.com>
2019-11-14 19:10:27 -0700

    Add code to the User, Role, UserRoles model classes

--- src/main/java/com/schoolwork/zoos/model/User.java
+++ src/main/java/com/schoolwork/zoos/model/User.java
@@ -1,4 +1,117 @@
 package com.schoolwork.zoos.model;
 
-public class User {
+// User is considered the parent entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    private String username;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("user")
+    private List<UserRoles> userroles = new ArrayList<>();
+
+    public User()
+    {
+    }
+
+    public User(String username,
+                String password,
+                String primaryemail,
+                List<UserRoles> userRoles)
+    {
+        setUsername(username);
+        setPassword(password);
+        for (UserRoles ur : userRoles)
+        {
+            ur.setUser(this);
+        }
+        this.userroles = userRoles;
+    }
+
+    public long getUserid()
+    {
+        return userid;
+    }
+
+    public void setUserid(long userid)
+    {
+        this.userid = userid;
+    }
+
+    public String getUsername()
+    {
+        if (username == null) // this is possible when updating a user
+        {
+            return null;
+        } else
+        {
+            return username.toLowerCase();
+        }
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username.toLowerCase();
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNotEncrypt(String password){
+        this.password = password;
+    }
+
+    public List<UserRoles> getUserroles()
+    {
+        return userroles;
+    }
+
+    public void setUserroles(List<UserRoles> userroles)
+    {
+        this.userroles = userroles;
+    }
+
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        for (UserRoles r : this.userroles)
+        {
+            String myRole = "ROLE_" + r.getRole()
+                    .getName()
+                    .toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+        return rtnList;
+    }
+
+    //changed password setters and this last JsonIgnore method
 }
