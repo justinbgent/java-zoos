eeb76f46b4765670ae23aa69c36757a80fae6537
Justin Gent <justinbgent@gmail.com>
2019-11-14 20:42:51 -0700

    Add all security config files and users now work

--- src/main/java/com/schoolwork/zoos/SeedData.java
+++ src/main/java/com/schoolwork/zoos/SeedData.java
@@ -25,13 +25,15 @@ public class SeedData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Role r1 = new Role("admin");
-        Role r2 = new Role("user");
-        Role r3 = new Role("data");
+        Role r1 = new Role("ADMIN");
+        Role r2 = new Role("ANIMALDATA");
+        Role r3 = new Role("ZOODATA");
+        Role r4 = new Role("MGR");
 
         r1 = roleRepo.save(r1);
         r2 = roleRepo.save(r2);
         r3 = roleRepo.save(r3);
+        r4 = roleRepo.save(r4);
 
         // admin, data, user
         ArrayList<UserRoles> admins = new ArrayList<>();
@@ -41,30 +43,43 @@ public class SeedData implements CommandLineRunner {
                 r2));
         admins.add(new UserRoles(new User(),
                 r3));
+        admins.add(new UserRoles(new User(),
+                r4));
         User u1 = new User("admin",
                 "password",
                 admins);
 
         userRepo.save(u1);
 
-        ArrayList<UserRoles> datas = new ArrayList<>();
-        datas.add(new UserRoles(new User(),
+        ArrayList<UserRoles> managers = new ArrayList<>();
+        managers.add(new UserRoles(new User(),
+                r4));
+        managers.add(new UserRoles(new User(),
                 r3));
-        datas.add(new UserRoles(new User(),
+        managers.add(new UserRoles(new User(),
                 r2));
-        User u2 = new User("data",
-                "data",
-                datas);
+        User u2 = new User("manager",
+                "manager",
+                managers);
 
         userRepo.save(u2);
 
-        ArrayList<UserRoles> users = new ArrayList<>();
-        users.add(new UserRoles(new User(),
-                r2));
-        User u3 = new User("user",
-                "user!",
-                users);
+        ArrayList<UserRoles> zoodata = new ArrayList<>();
+        zoodata.add(new UserRoles(new User(),
+                r3));
+        User u3 = new User("zoodata",
+                "zoodata!",
+                zoodata);
 
         userRepo.save(u3);
+
+        ArrayList<UserRoles> animaldata = new ArrayList<>();
+        animaldata.add(new UserRoles(new User(),
+                r2));
+        User u4 = new User("animaldata",
+                "animaldata!",
+                animaldata);
+
+        userRepo.save(u4);
     }
 }
